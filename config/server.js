93cd3cf0c7bb1ad6ad702a544ef6f3f34d96acc1
huzaifa <khanhuzaifa207@gmail.com>
2021-07-29 15:19:25 +0500

    first init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '48eb263d30ee0c8614acba91cd001e83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ba04e884-e3b1-486b-9be0-9e17293239ed'
+};
