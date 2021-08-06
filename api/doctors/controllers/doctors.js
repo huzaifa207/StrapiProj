7dd065e4c438ce82d6b69ec0cff624afe67700f6
huzaifa <khanhuzaifa207@gmail.com>
2021-08-06 14:11:01 +0500

    added qs library

--- api/doctors/controllers/doctors.js
+++ api/doctors/controllers/doctors.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  findByCity: async (ctx) => {
+    let entities = [];
+    let { city } = ctx.params;
+
+    console.log(strapi.services.doctors.find);
+    entities = await strapi.services.doctors.find();
+
+    let filtered_entities = await entities.filter(
+      (entity) => entity.contactDetails && entity.contactDetails.city === city
+    );
+
+    return filtered_entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.doctors })
+    );
+  },
+};
--- api/doctors/services/doctors.js
+++ api/doctors/services/doctors.js
@@ -1,8 +1,16 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  searchCity: async (params) => {
+    let results = await strapi
+      .query("doctors")
+      .find([{ path: "contactDetails", populate: { path: "city" } }]);
+
+    return results;
+  },
+};
